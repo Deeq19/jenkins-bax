539af45dc826a400489ff8d927d6cbbfe0856ea6
tylersul <57144394+tylersul@users.noreply.github.com>
2019-10-29 10:05:17 -0500

    Adding copyfile PS script.
    Initial script to test Jenkins clone.

---
+++ CopyFile.ps1
@@ -0,0 +1,38 @@
+﻿#Test Jenkins-Powershell parameter push 
+
+# Summary: this function requests a Target path, Target File, and a Destination Path. 
+#The file requested (TargetFile) will be copied to the DestinationPath provided. 
+#-------------------------
+
+ function CopyFile{
+
+    Param (
+        #[Parameter (Mandatory=$true)] [STRING] $Region,
+        #[Parameter (Mandatory=$true)] [STRING] $Env,
+        #[STRING] $Env,
+        #[Parameter (Mandatory=$true)] [STRING] $ScriptName, #$ScriptName 
+        #[Parameter (Mandatory=$true)] [STRING] $Server_Name,
+        #[STRING] $Server
+        #[STRING] $Services
+        #[STRING] $TargetPath
+
+
+        [Parameter (Mandatory=$true)] [STRING] $TargetPath,
+        #[STRING] $TargetFile
+        [Parameter (Mandatory=$true)] [STRING] $TargetFile,
+        [Parameter (Mandatory=$true)] [STRING] $DestPath
+        #[Parameter (Mandatory=$true)] [STRING] $DestFile
+        )
+
+    $TargetPath = "$($TargetPath)\$($TargetFile)"
+    #Return $TargetPath
+
+    $DestinationPath = "$($DestPath)"
+    #Return $DestinationPath 
+    
+    Copy-Item $TargetPath -Destination $DestinationPath
+    Write-Output "File" $TargetFile "has been copied to" $DestPath
+
+
+}
+CopyFile
