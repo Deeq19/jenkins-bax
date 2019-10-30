32f1c615b67524d82a1e1fd0dc383d19113ac333
tylersul <57144394+tylersul@users.noreply.github.com>
2019-10-30 15:15:27 -0500

    Adding second deliverable script.
    SearchError

---
+++ SearchError.ps1
@@ -0,0 +1,53 @@
+﻿#Test Jenkins-Powershell parameter push 
+
+# Summary: this script searches for a prodived error (ie 12345) in all the .txt files in the provided path. 
+
+#Example: I have a folder with 5 text files, 3 of which have '12345' somewhere within the file. Two of those files do not have any errors.
+#         The script will return the name of the files where the error was found (ie. Test1.txt, Test2.txt) 
+#-------------------------
+
+ function SearchError{
+
+    Param (
+        [Parameter (Mandatory=$true)] [STRING] $SourcePath,
+        #[Parameter (Mandatory=$true)] [STRING] $SourceFile,
+        [Parameter (Mandatory=$true)] [STRING] $ErrorCode,
+        #[Parameter (Mandatory=$true)] [STRING] $FileType
+        #[Parameter (Mandatory=$true)] [STRING] $DestPath
+        
+        #for testing purposes. Where you want to see the output file.Important to add .\NAMEOFTXTFILE.txt
+        [Parameter (Mandatory=$true)] [STRING] $OutputSummary #example path: C:\Users\jessica.gutierrez\Desktop\FunctionOutputs\CopyFile_OutputSummary.txt
+      
+
+        )
+    
+    $TargetPath = "$($SourcePath)"
+    #$TargetPath = "$($SourcePath)\$($SourceFile)"
+    #Return $TargetPath
+
+    $DestinationPath = "$($DestPath)"
+    #Return $DestinationPath 
+   
+
+    #foreach($error in $TargetPath) {
+
+    $Files = Get-ChildItem $TargetPath | Select-String -pattern $ErrorCode | group path | select name
+    #Get-ChildItem $TargetPath | Select-String -pattern $ErrorCode #return text with error code 
+    Write-Output ( "Files in " + $TargetPath + " were search for error code: " + $ErrorCode + $Files)| Out-File $OutputSummary
+    Write-Output ("Open " + $OutputSummary + " to see script output" )
+    $Files
+
+
+
+}
+SearchError 
+
+
+ #[Parameter (Mandatory=$true)] [STRING] $Region,
+        #[Parameter (Mandatory=$true)] [STRING] $Env,
+        #[STRING] $Env,
+        #[Parameter (Mandatory=$true)] [STRING] $ScriptName, #$ScriptName 
+        #[Parameter (Mandatory=$true)] [STRING] $Server_Name,
+        #[STRING] $Server
+        #[STRING] $Services
+        #[STRING] $TargetPath
