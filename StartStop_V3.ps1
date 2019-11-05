2c8f3e214ade2c618e7c610603c6bfeb0eea3af1
tylersul <57144394+tylersul@users.noreply.github.com>
2019-11-05 16:34:48 -0600

    update.

--- StartStop_V3.ps1
+++ StartStop_V3.ps1
@@ -1,12 +1,11 @@
-﻿function Start_Stop{
+function Start_Stop{
     Param(
 
-        [Parameter (Mandatory=$true)] [String] $Start_Stop,
         [Parameter (Mandatory=$true)] [String]$Server,
         [String]$Services
 
         )
-    Write-Output $Start_Stop
+        
     Write-Output $Server
     Write-Output $Services
 
