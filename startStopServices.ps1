83b3c552351ecf56218dff2a8978ad9f1f1f5fc2
Deeq19 <57427839+Deeq19@users.noreply.github.com>
2019-11-05 22:18:19 -0600

    Creating new ps file

---
+++ startStopServices.ps1
@@ -0,0 +1,27 @@
+function startStopServices{
+
+ Param(
+        [Parameter (Mandatory=$true)] [String] $startStop,
+        [Parameter (Mandatory=$true)] [String] $server, #USOHCOGAPP901
+        [String[]] $services) #DISP0X
+
+        
+   
+    [String] $sortOrder = ($startStop + "_Order")
+    [String] $waitTime = ($startStop + "_Wait")
+    # Write-Output ($services -Join ",")
+        
+    $Sourcefile = "C:\Users\deeq\Downloads\Cognos_ControlFile.csv"
+    $StartProperties = Import-Csv $Sourcefile | Where-Object { ($_.Server_Name -eq $server) -and ($_.Service_Code -match ($services -Join "|"))} `
+                                               | Select Server_Name, Service_Code, Service_Name, Start_Order, Stop_Order, Start_Flag, Start_Wait, Stop_Wait `
+                                               | Sort-Object -Property {[int]$_.$sortOrder}
+    $StartProperties
+    
+    foreach($line in $StartProperties){
+       Write-Output ($startStop `
+                + " " + [string]$line.'Service_Name' `
+                + " " + " Timeout " + [string]$line.$waitTime)
+    }
+}
+
+
